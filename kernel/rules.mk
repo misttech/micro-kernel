c4ce749509e9a53b8813887b0f5d2a1cbe02a511
Bruno Herrera <bruno.herrera@mist-os.com>
2025-02-12 11:05:13 -0300

    [mk] Use a pragma to set all objects to hidden

--- kernel/rules.mk
+++ kernel/rules.mk
@@ -1,3 +1,10 @@
+# Copyright 2025 Mist Tecnologia Ltda
+# Copyright (c) 2008-2015 Travis Geiselbrecht
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 MODULE := $(LOCAL_DIR)
@@ -23,6 +30,8 @@ MODULE_DEPS += kernel/vm
 else
 MODULE_DEPS += kernel/novm
 endif
+
+MODULE_COMPILEFLAGS += -include kernel/hidden.h
 
 MODULE_OPTIONS := extra_warnings
 
