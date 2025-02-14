14cc6edd9fecb908053117dc795e72c83b7fda92
Daniel Volpato <volpato.swe@gmail.com>
2025-02-14 17:57:08 -0300

    [mk] build lib fit

---
+++ mk/lib/fit/rules.mk
@@ -0,0 +1,11 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_SRCS += $(LOCAL_DIR)/fit.cc
+
+# ifeq ($(call TOBOOL,WITH_TESTS),true)
+# MODULE_DEPS += $(LOCAL_DIR)/test
+# endif
+
+include make/module.mk
