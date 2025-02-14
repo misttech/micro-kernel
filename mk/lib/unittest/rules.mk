d860fc158f0b71c67644b1bafc647ffbf108efac
Daniel Volpato <volpato.swe@gmail.com>
2025-02-14 17:21:30 -0300

    wip! [mk] port unittest from fuchsia f12ab51693a8567ff16c6f8234b520ea65f2ce59

--- mk/lib/unittest/rules.mk
+++ mk/lib/unittest/rules.mk
@@ -3,7 +3,12 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 MODULE := $(LOCAL_DIR)
 
 MODULE_SRCS := \
-	$(LOCAL_DIR)/unittest.c \
-	$(LOCAL_DIR)/all_tests.c \
+	$(LOCAL_DIR)/console.cc \
+	$(LOCAL_DIR)/phys.cc \
+  $(LOCAL_DIR)/unittest.cc \
+  $(LOCAL_DIR)/user_memory_tests.cc \
+  $(LOCAL_DIR)/user_memory.cc \
+
+# MODULE_DEPS += lib/fit
 
 include make/module.mk
